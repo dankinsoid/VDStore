07d6f89d13b753362a0451c2961082bc316a22f5
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2023-08-21 00:53:54 +0300

    hm

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -15,9 +16,24 @@ let package = Package(
         .library(name: "VDStore", targets: ["VDStore"]),
     ],
     dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            branch: "main"
+        ),
     ],
     targets: [
-        .target(name: "VDStore"),
+        .target(name: "VDStore", dependencies: ["VDStoreMacros"]),
         .testTarget(name: "VDStoreTests", dependencies: ["VDStore"]),
+        .macro(
+            name: "VDStoreMacros",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftOperators", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]
+        ),
     ]
 )
